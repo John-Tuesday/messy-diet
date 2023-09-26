22ee9b1a4fd131c32e27b058e995c9c5beb7ac53
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-09-26 14:23:42 -0500

    Cleanup the Adjustable Serving Size.
    It's ugly but functional

--- feature/meal/presentation/src/main/kotlin/org/calamarfederal/messydiet/feature/meal/presentation/view/ViewMealUi.kt
+++ feature/meal/presentation/src/main/kotlin/org/calamarfederal/messydiet/feature/meal/presentation/view/ViewMealUi.kt
@@ -1,16 +1,29 @@
 package org.calamarfederal.messydiet.feature.meal.presentation.view
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import android.icu.number.LocalizedNumberFormatter
+import android.icu.number.NumberFormatter
+import android.icu.number.Precision
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -18,7 +31,9 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.calamarfederal.messydiet.diet_data.model.Nutrition
 import org.calamarfederal.messydiet.diet_data.model.Portion
 import org.calamarfederal.messydiet.feature.meal.data.model.Meal
-import org.calamarfederal.messydiet.feature.measure.NutritionInfoColumn
+import org.calamarfederal.messydiet.feature.measure.*
+import org.calamarfederal.messydiet.measure.VolumeUnit
+import org.calamarfederal.messydiet.measure.WeightUnit
 import org.calamarfederal.messydiet.measure.grams
 import org.calamarfederal.messydiet.measure.milliliters
 
@@ -97,10 +112,11 @@ private fun ViewMealInnerLayout(
             text = meal.name,
             style = nameStyle,
         )
+
+        fun extractPortion() = (meal.portion.weight?.inGrams() ?: meal.portion.volume?.inMilliliters())!!.toFloat()
+
         var portionState by remember(meal) {
-            mutableFloatStateOf(
-                (meal.portion.weight?.inGrams() ?: meal.portion.volume?.inMilliliters())!!.toFloat()
-            )
+            mutableFloatStateOf(extractPortion())
         }
 
         val adjustedNutrition by remember(meal) {
@@ -113,24 +129,171 @@ private fun ViewMealInnerLayout(
                 (Nutrition() + meal).scaleToPortion(portion)
             }
         }
-        Slider(
-            value = portionState,
-            onValueChange = { portionState = it },
-            valueRange = 0f..1000f,
-            steps = 1001 / 5,
-        )
         Surface(
             shape = MaterialTheme.shapes.small,
             tonalElevation = 1.dp,
         ) {
-            NutritionInfoColumn(
-                nutrition = adjustedNutrition,
+            Column(
                 modifier = Modifier
-                    .padding(
-                        horizontal = 4.dp,
-                        vertical = 2.dp,
+                    .padding(4.dp)
+            ) {
+                val nutritionInfoStyle = NutrientInfoTextStyle.default()
+                val portionUnitString = when {
+                    meal.portion.volume != null -> VolumeUnit.Milliliter.labelString
+                    meal.portion.weight != null -> WeightUnit.Gram.labelString
+                    else -> throw (NoServingSizeSpecified())
+                }
+
+                AdjustableServingSize(
+                    sliderValue = portionState,
+                    onSliderValueChange = { portionState = it },
+                    onResetSliderValue = { portionState = extractPortion() },
+                    valueRange = 1f..1_000f,
+                    steps = 1_000,
+                    unitString = portionUnitString,
+                    labelStyle = nutritionInfoStyle.servingSizeLabelStyle,
+                    numberStyle = nutritionInfoStyle.servingSizeValueStyle,
+                    unitStyle = nutritionInfoStyle.servingSizeValueStyle,
+                )
+                NutritionInfoColumn(
+                    nutrition = adjustedNutrition,
+                    hidePortion = true,
+                    textStyles = nutritionInfoStyle,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun AdjustableServingSize(
+    sliderValue: Float,
+    onSliderValueChange: (Float) -> Unit,
+    onResetSliderValue: () -> Unit,
+    unitString: String,
+    modifier: Modifier = Modifier,
+    valueRange: ClosedFloatingPointRange<Float> = 0f..1_000f,
+    steps: Int = 0,
+    shape: Shape = MaterialTheme.shapes.small,
+    numberFormatter: LocalizedNumberFormatter = NumberFormatter
+        .withLocale(LocalConfiguration.current.locales[0])
+        .precision(Precision.integer()),
+    labelStyle: TextStyle = LocalTextStyle.current,
+    numberStyle: TextStyle = LocalTextStyle.current,
+    unitStyle: TextStyle = LocalTextStyle.current,
+) {
+    var inputMode by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+    val isEditMode by remember { derivedStateOf { inputMode == 1 } }
+    val isViewOnlyMode by remember { derivedStateOf { inputMode == 0 } }
+    fun toEditMode() {
+        inputMode = 1
+    }
+
+    fun toViewOnlyMode() {
+        inputMode = 0
+    }
+
+    val textFieldFocusRequester = remember { FocusRequester() }
+    Surface(
+        modifier = modifier,
+        shape = shape,
+        tonalElevation = LocalAbsoluteTonalElevation.current + 1.dp,
+        onClick = { if (isEditMode) toViewOnlyMode() else toEditMode() },
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    text = "Serving Size",
+                    style = labelStyle,
+                    modifier = Modifier
+                        .alignByBaseline()
+                        .padding(end = 16.dp)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                if (isViewOnlyMode) {
+                    Text(
+                        text = numberFormatter.format(sliderValue).toString(),
+                        style = numberStyle,
+                        modifier = Modifier.alignByBaseline()
                     )
-            )
+                    Spacer(modifier = Modifier.width(1.dp))
+                    Text(
+                        text = unitString,
+                        style = unitStyle,
+                        modifier = Modifier.alignByBaseline()
+                    )
+                } else {
+                    var inputText by rememberSaveable(sliderValue) {
+                        mutableStateOf(numberFormatter.format(sliderValue).toString())
+                    }
+                    OutlinedTextField(
+                        value = inputText,
+                        onValueChange = { inputString ->
+                            inputText = inputString
+                            inputString.toFloatOrNull()?.let { onSliderValueChange(it) }
+                        },
+                        textStyle = numberStyle.copy(textAlign = TextAlign.End),
+                        readOnly = true,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        keyboardActions = KeyboardActions { toViewOnlyMode() },
+                        suffix = { Text(text = unitString) },
+                        modifier = Modifier
+                            .alignByBaseline()
+                            .focusRequester(textFieldFocusRequester)
+                    )
+
+                    LaunchedEffect(Unit) {
+                        textFieldFocusRequester.requestFocus()
+                    }
+                }
+            }
+            if (isEditMode) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+//                    FilledTonalIconButton(onClick = { textFieldFocusRequester.requestFocus() }) {
+//                        Icon(painterResource(id = R.drawable.keyboard), null)
+//                    }
+                    TextButton(onClick = { onResetSliderValue() }) {
+                        Icon(
+
+                            Icons.Default.Refresh,
+                            null
+                        )
+                    }
+                    Slider(
+                        value = sliderValue,
+                        onValueChange = onSliderValueChange,
+                        valueRange = valueRange,
+                        steps = steps,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(horizontal = 8.dp)
+                    )
+                    FilledTonalButton(onClick = { toViewOnlyMode() }) {
+                        Icon(Icons.Outlined.ArrowDropDown, null, modifier = Modifier.rotate(180f))
+                    }
+                }
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                HorizontalDivider(modifier = Modifier.weight(1f))
+                Text(
+                    text = "Preview",
+                    modifier = Modifier.padding(horizontal = 4.dp)
+                )
+                HorizontalDivider(modifier = Modifier.weight(1f))
+            }
         }
     }
 }
@@ -138,9 +301,17 @@ private fun ViewMealInnerLayout(
 @Preview
 @Composable
 private fun ViewMealPreview() {
-
     ViewMealScreenLayout(
-        meal = Meal(name = "Test Name"),
+        meal = Meal(
+            name = "Test Name",
+            portion = Portion(1.grams)
+        ),
         onNavigateUp = {},
     )
+}
+
+@Preview
+@Composable
+private fun ViewMealAdjustableServingSize() {
+
 }
